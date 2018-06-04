a11967a3086f0ddac456ae7d74fb74a9a18cb811
Jeromy <jeromyj@gmail.com>
2018-06-04 02:36:38 -0700

    make errors easier to figure out

--- const.go
+++ const.go
@@ -5,52 +5,60 @@ import (
 	"fmt"
 )
 
+type YamuxError struct {
+	msg string
+}
+
+func (ye YamuxError) Error() string {
+	return ye.msg
+}
+
 var (
 	// ErrInvalidVersion means we received a frame with an
 	// invalid version
-	ErrInvalidVersion = fmt.Errorf("invalid protocol version")
+	ErrInvalidVersion = &YamuxError{"invalid protocol version"}
 
 	// ErrInvalidMsgType means we received a frame with an
 	// invalid message type
-	ErrInvalidMsgType = fmt.Errorf("invalid msg type")
+	ErrInvalidMsgType = &YamuxError{"invalid msg type"}
 
 	// ErrSessionShutdown is used if there is a shutdown during
 	// an operation
-	ErrSessionShutdown = fmt.Errorf("session shutdown")
+	ErrSessionShutdown = &YamuxError{"session shutdown"}
 
 	// ErrStreamsExhausted is returned if we have no more
 	// stream ids to issue
-	ErrStreamsExhausted = fmt.Errorf("streams exhausted")
+	ErrStreamsExhausted = &YamuxError{"streams exhausted"}
 
 	// ErrDuplicateStream is used if a duplicate stream is
 	// opened inbound
-	ErrDuplicateStream = fmt.Errorf("duplicate stream initiated")
+	ErrDuplicateStream = &YamuxError{"duplicate stream initiated"}
 
 	// ErrReceiveWindowExceeded indicates the window was exceeded
-	ErrRecvWindowExceeded = fmt.Errorf("recv window exceeded")
+	ErrRecvWindowExceeded = &YamuxError{"recv window exceeded"}
 
 	// ErrTimeout is used when we reach an IO deadline
-	ErrTimeout = fmt.Errorf("i/o deadline reached")
+	ErrTimeout = &YamuxError{"i/o deadline reached"}
 
 	// ErrStreamClosed is returned when using a closed stream
-	ErrStreamClosed = fmt.Errorf("stream closed")
+	ErrStreamClosed = &YamuxError{"stream closed"}
 
 	// ErrUnexpectedFlag is set when we get an unexpected flag
-	ErrUnexpectedFlag = fmt.Errorf("unexpected flag")
+	ErrUnexpectedFlag = &YamuxError{"unexpected flag"}
 
 	// ErrRemoteGoAway is used when we get a go away from the other side
-	ErrRemoteGoAway = fmt.Errorf("remote end is not accepting connections")
+	ErrRemoteGoAway = &YamuxError{"remote end is not accepting connections"}
 
 	// ErrConnectionReset is sent if a stream is reset. This can happen
 	// if the backlog is exceeded, or if there was a remote GoAway.
-	ErrConnectionReset = fmt.Errorf("connection reset")
+	ErrConnectionReset = &YamuxError{"stream reset"}
 
 	// ErrConnectionWriteTimeout indicates that we hit the "safety valve"
 	// timeout writing to the underlying stream connection.
-	ErrConnectionWriteTimeout = fmt.Errorf("connection write timeout")
+	ErrConnectionWriteTimeout = &YamuxError{"connection write timeout"}
 
 	// ErrKeepAliveTimeout is sent if a missed keepalive caused the stream close
-	ErrKeepAliveTimeout = fmt.Errorf("keepalive timeout")
+	ErrKeepAliveTimeout = &YamuxError{"keepalive timeout"}
 )
 
 const (
