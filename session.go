3ffd0464aedfd2256fb52d3532306fc15b04d08c
xuqiang <xuqiang76@163.com>
2019-09-10 18:36:31 +0800

    fix pingRTT

--- session.go
+++ session.go
@@ -75,9 +75,9 @@ type Session struct {
 	shutdownCh   chan struct{}
 	shutdownLock sync.Mutex
 
-	// if the connection alive then RTT > 0 , else wrongRTT
-	// the fisrt Ping() return value equal this RTT
-	RTT time.Duration
+	// if the connection alive then pingRTT > 0 , else wrongRTT
+	// UnixNano
+	pingRTT time.Duration
 }
 
 // sendReady is used to either mark a stream as ready
@@ -120,6 +120,12 @@ func newSession(config *Config, conn io.ReadWriteCloser, client bool) *Session {
 		go s.keepalive()
 	}
 	return s
+}
+
+// if the connection alive then return > 0 , else 0
+// UnixNano
+func (s *Session) GetRTT() time.Duration {
+	return s.pingRTT
 }
 
 // IsClosed does a safe check to see if we have shutdown
@@ -230,7 +236,7 @@ func (s *Session) Close() error {
 	s.shutdownLock.Lock()
 	defer s.shutdownLock.Unlock()
 
-	defer func(){s.RTT = wrongRTT}()
+	defer func(){s.pingRTT = wrongRTT}()
 
 	if s.shutdown {
 		return nil
@@ -292,29 +298,28 @@ func (s *Session) Ping() (time.Duration, error) {
 	hdr := header(make([]byte, headerSize))
 	hdr.encode(typePing, flagSYN, 0, id)
 	if err := s.waitForSend(hdr, nil); err != nil {
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return 0, err
 	}
 
 	// Wait for a response
-	start := time.Now()
+	start := time.Now().UnixNano()
 	select {
 	case <-ch:
 	case <-time.After(s.config.ConnectionWriteTimeout):
 		s.pingLock.Lock()
 		delete(s.pings, id) // Ignore it if a response comes later.
 		s.pingLock.Unlock()
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return 0, ErrTimeout
 	case <-s.shutdownCh:
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return 0, ErrSessionShutdown
 	}
 
 	// Compute the RTT
-	tmpRtt := time.Now().Sub(start)
-	s.RTT = tmpRtt
-	return tmpRtt, nil
+	s.pingRTT = time.Duration(time.Now().UnixNano() - start + 1)
+	return s.pingRTT / 1e9, nil
 }
 
 // keepalive is a long running goroutine that periodically does
@@ -363,22 +368,22 @@ func (s *Session) waitForSendErr(hdr header, body io.Reader, errCh chan error) e
 	select {
 	case s.sendCh <- ready:
 	case <-s.shutdownCh:
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return ErrSessionShutdown
 	case <-timer.C:
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return ErrConnectionWriteTimeout
 	}
 
 	select {
 	case err := <-errCh:
-		//s.RTT = wrongRTT
+		//s.pingRTT = wrongRTT
 		return err
 	case <-s.shutdownCh:
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return ErrSessionShutdown
 	case <-timer.C:
-		s.RTT = wrongRTT
+		s.pingRTT = wrongRTT
 		return ErrConnectionWriteTimeout
 	}
 }
